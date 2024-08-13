ebc324c5aaf6cfcb0913c2f195cd8962dcb598b6
MATEBOOK-13\jared <jared0192galicia@gmail.com>
2024-08-13 17:37:15 -0600

    Add icons animation

--- components/section/about.tsx
+++ components/section/about.tsx
@@ -3,12 +3,22 @@ import 'tailwindcss/tailwind.css';
 // import ''
 
 export default function About(props: any) {
+  const bottomFromTop = 'h-full';
   return (
     <>
       <section className="w-1/2 h-[400px] bg-slate-300 flex items-center justify-center">
-        <ul className="flex flex-col items-center space-y-4">
-          <li className="overflow-auto max-w-10 max-h-10 duration-1000 animate-spin-fast ">
-            <img src={'/icon/react.png'} alt={'alt'} className="w-10 h-10" />
+        <ul className="flex flex-row items-center space-y-4 border border-cyan-800 w-full h-full">
+          <li className={cn("overflow-auto w-full h-full duration-1000 animate-slide-fade", bottomFromTop)}>
+            <img src={'/icon/react.png'} alt={'alt'} className="w-10 h-10 animate-spin-fast" />
+          </li>
+          <li className={cn("overflow-auto w-full h-full duration-1000 animate-slide-fade", bottomFromTop)}>
+            <img src={'/icon/hono.png'} alt={'alt'} className="w-10 h-10 animate-spin-fast" />
+          </li>
+          <li className={cn("overflow-auto w-full h-full duration-1000 animate-slide-fade", bottomFromTop)}>
+            <img src={'/icon/node.png'} alt={'alt'} className="w-10 h-10 animate-spin-fast" />
+          </li>
+          <li className={cn("overflow-auto w-full h-full duration-1000 animate-slide-fade", bottomFromTop)}>
+            <img src={'/icon/bun.png'} alt={'alt'} className="w-10 h-10 animate-spin-fast" />
           </li>
         </ul>
       </section>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,17 +14,25 @@ const config: Config = {
       },
       keyframes: {
         slideUpRotate: {
-          '0%': { transform: 'translateY(100px) rotate(0deg)', opacity: '0' },
+          '0%': { transform: 'translateY(400px) rotate(0deg)', opacity: '0' },
           '100%': { transform: 'translateY(0) rotate(360deg)', opacity: '1' },
         },
         spin: {
           '0%': { transform: 'rotate(0deg)' },
           '100%': { transform: 'rotate(360deg)' },
         },
+        slideFade: {
+          '0%': { transform: 'translateY(95%)', opacity: '0' }, // Empieza fuera del contenedor y oculto
+          '35%': {  opacity: '1' }, // Subida inicial
+          '75%': { opacity: '1' }, // Cerca de la parte superior
+          '100%': { transform: 'translateY(-2%)', opacity: '0' }, // Sale del contenedor y desaparece
+        },
       },
       animation: {
         slideUpRotate: 'slideUpRotate 1s ease-in-out forwards',
         'spin-fast': 'spin 2.5s linear infinite',
+        'slide-fade': 'slideFade 8s ease-in-out infinite',
+
       },
     },
   },
