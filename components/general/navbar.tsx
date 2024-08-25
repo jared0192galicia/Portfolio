86af942bfeb68282e36b6485627d805052b23558
MATEBOOK-13\jared <jared0192galicia@gmail.com>
2024-08-25 16:19:00 -0600

    add secction for about

--- components/general/navbar.tsx
+++ components/general/navbar.tsx
@@ -8,49 +8,55 @@ export default function Navbar({ className, send }: any) {
   const [selected, setSelected] = useState<
     "About" | "Experience" | "Contact" | "Skills"
   >("About");
-  const baseItemClasses = "w-1/3 sm:w-1/4 text-center py-5";
+  const baseItemClasses = "w-1/3 sm:w-1/4 text-center pt-5 pb-2";
   const textClass = `text-cyan-500`;
   const hoverClass =
     "hover:text-fuchsia-500 hover:cursor-pointer transition-colors duration-500";
-  const selectedClass: string = "bg-gradient-to-b from-gray-800 to- rounded-sm";
-  // const [state, send] = useMachine(navigationMachine);
+  const selectedClass: string = "border-b-2 border-fuchsia-500 roundedmd";
 
   return (
     <>
       <ul className={cn("flex flex-row xl:px-20", className)}>
         <li
           className={cn(baseItemClasses, textClass, hoverClass, {
-            "text-red-600": selected == "About",
+            "text-white": selected == "About",
             [selectedClass]: selected == "About",
           })}
-          onClick={() => send({ type: "GO_TO_ABOUT" })}
+          onClick={() => {
+            send({ type: "GO_TO_ABOUT" });
+            setSelected("About");
+          }}
         >
           Sobre mi
         </li>
         <li
           className={cn(baseItemClasses, textClass, hoverClass, {
-            "text-red-600": selected == "Skills",
+            "text-white": selected == "Skills",
             [selectedClass]: selected == "Skills",
           })}
-          onClick={() => send({ type: "GO_TO_SKILLS" })}
+          onClick={() => {
+            send({ type: "GO_TO_SKILLS" });
+            setSelected("Skills");
+          }}
         >
           Habilidades
         </li>
         <li
           className={cn(baseItemClasses, textClass, hoverClass, {
-            "text-red-600": selected == "Experience",
+            "text-white": selected == "Experience",
             [selectedClass]: selected == "Experience",
           })}
           // onClick={() => setSelected("Experience")}
           onClick={() => {
             send({ type: "GO_TO_EXPERIENCE" });
+            setSelected('Experience');
           }}
         >
           Experiencia
         </li>
         <li
           className={cn(baseItemClasses, textClass, hoverClass, {
-            "text-red-600": selected == "Contact",
+            "text-white": selected == "Contact",
             [selectedClass]: selected == "Contact",
           })}
           onClick={() => setSelected("Contact")}
--- components/section/about.tsx
+++ components/section/about.tsx
@@ -1,12 +1,33 @@
+import cn from "@/service/clsx";
+
 export default function About() {
+  const baseClassName: string =
+    "text-justify text-white p-5 py-0 font-light text-sm";
+  const headingClassName: string = "text-2xl font-bold mb-4 text-center";
+  const paragraphClassName: string = "mb-4 leading-relaxed";
+
   return (
-    <section>
-      <p className="text-white max-wscreen w-full p-5 font-extralight text-justify cursor-default">
+    <section className="py-8">
+      <h2 className={cn(baseClassName, headingClassName)}>
+        ¡Hola! Te cuento sobre mí
+      </h2>
+      <p className={cn(baseClassName, paragraphClassName)}>
         Soy Jared Galicia, un apasionado desarrollador web con experiencia en la
         creación de aplicaciones robustas y eficientes tanto en frontend como en
         backend. Me especializo en el uso de tecnologías modernas como React,
         TypeScript, Node.js, y más recientemente, he trabajado con Bun.js y Hono
         para optimizar el rendimiento de mis aplicaciones.
+      </p>
+      <p className={cn(baseClassName, paragraphClassName)}>
+        Constantemente me gusta investigar acerca de nuevas tecnologías para
+        potenciar mis aplicaciones. Mi objetivo es siempre estar al tanto de las
+        últimas tendencias y herramientas que puedan mejorar mi trabajo, sin
+        entorpecer el avance sobre mis proyectos actuales.
+      </p>
+      <p className={cn(baseClassName, paragraphClassName)}>
+        Además de la programación, disfruto mantenerme activo físicamente y me
+        apasionan los videojuegos. Siempre estoy buscando un equilibrio entre mi
+        vida profesional y mis pasatiempos para mantenerme motivado y saludable.
       </p>
     </section>
   );
